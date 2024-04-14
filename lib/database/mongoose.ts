84a6553c87d700967187eb470bf1a5e0e3eab151
CoderHArsit <hs8449865663@gmail.com>
2024-04-15 00:27:23 +0530

    some bug fixes

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -11,12 +11,13 @@ interface MongooseConnection{
     promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection=(global as any).mongoose
+let cached: MongooseConnection=(global as any).mongoose;
 
 if((!cached)){
     cached=(!global as any).mongoose={
         conn:null,promise:null
-    }
+    };
+    cached=(global as any).mongoose;
 }
 
 export const connectToDatabase=async()=>{
