9123cdce84ce532427106afb4ed24903be2244e3
CoderHArsit <hs8449865663@gmail.com>
2024-04-15 00:09:20 +0530

    implementes clerk with webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+publicRoutes:['/api/webhooks/clerk']
+//this means we are allowing public access to this url
+});
 
 export const config={
     matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
