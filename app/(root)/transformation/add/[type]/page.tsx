023131aa8083cbaf1e90537ac28c75e7f86c8821
CoderHArsit <hs8449865663@gmail.com>
2024-04-17 23:04:21 +0530

    some pages added

--- app/(root)/transformation/add/[type]/page.tsx
+++ app/(root)/transformation/add/[type]/page.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { getUserById } from '@/lib/actions/user.actions';
 
-const AddTransformationPage = () => {
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+const AddTransformationPage = async ({params:{type}}:SearchParamProps) => {
+  const transformation=transformationTypes[type];
+  const {userId}=auth();
+  if(!userId) redirect('sign-in')
+  const user=await getUserById(userId);
   return (
-    <div>
-      AddTransformation
-    </div>
+    <>
+    <Header
+    title={transformation.title}
+    subtitle={transformation.subTitle}
+    />
+    <TransformationForm
+     action='Add'
+     userId={user._id}
+     type={transformation.type as TransformationTypeKey}
+     creditBalance={user.creditBalance}
+    />
+    </>
   )
 }
 
